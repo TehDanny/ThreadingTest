18fd046c9f3f9ce515f70211a5dc577b3003a3cf
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-16 13:49:35 +0200

    Added setup for TestMethod(). It crashes.

--- ThreadingTest/MainWindow.xaml.cs
+++ ThreadingTest/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,32 @@ namespace ThreadingTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int testInt;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void WindowKeyDown(object sender, KeyEventArgs e)
+        {
+            TestMethod();
+        }
+
+        private void TestMethod()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                testInt++;
+                TestLabel.Content = testInt;
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            TestMethod();
         }
     }
 }
